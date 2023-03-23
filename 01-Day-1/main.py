8f363de72c1c59e613d87f42386bf843620227c9
mjalalimanesh <mohamad.manesh@gmail.com>
2023-03-23 18:17:37 +0430

    Add day 1

--- 01-Day-1/main.py
+++ 01-Day-1/main.py
@@ -1,16 +1,8 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Create greeting for the program
+print("Welcome to the Band Name Generator!")
+# Get city name from user
+city = input("What's the name of the city you grew up in?\n")
+# Get pet name from user
+pet_name = input("What's you pet name?\n")
+# Combine city name and pet name to create a name for the band
+print("Your band name could be " + city + " " + pet_name)
